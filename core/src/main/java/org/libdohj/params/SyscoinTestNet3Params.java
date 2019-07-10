aea4be613682cdbf98c8c2438bbb0c503dbd94b5
sidhujag <sidhujag@gmail.com>
2019-07-09 22:48:48 -0700

    include utils

--- core/src/main/java/org/libdohj/params/SyscoinTestNet3Params.java
+++ core/src/main/java/org/libdohj/params/SyscoinTestNet3Params.java
@@ -24,7 +24,7 @@ import org.bitcoinj.core.StoredBlock;
 import org.bitcoinj.core.VerificationException;
 import org.bitcoinj.store.BlockStore;
 import org.bitcoinj.store.BlockStoreException;
-
+import org.bitcoinj.core.Utils;
 import static com.google.common.base.Preconditions.checkState;
 import java.math.BigInteger;
 /**
