924ccdf3ecd220417d765a2f691a3887ff4adcd5
sidhujag <jsidhu@blockchainfoundry.co>
2020-02-27 15:21:08 -0800

    update checkpoints, bitcoinj to 0.15.6

--- core/src/main/java/org/libdohj/params/SyscoinMainNetParams.java
+++ core/src/main/java/org/libdohj/params/SyscoinMainNetParams.java
@@ -79,6 +79,9 @@ public class SyscoinMainNetParams extends AbstractSyscoinParams {
         checkpoints.put(    240000, Sha256Hash.wrap("906918ba0cbfbd6e4e4e00d7d47d08bef3e409f47b59cb5bd3303f5276b88f0f"));
         checkpoints.put(    280000, Sha256Hash.wrap("651375427865345d37a090ca561c1ed135c6b8dafa591a59f2abf1eb26dfd538"));
         checkpoints.put(    292956, Sha256Hash.wrap("ae6dca1b9dd7adcb8a11c8ea7f9fe72bb47ff6e4156e1d172e2a8612b18a319d"));
+        checkpoints.put(    350000, Sha256Hash.wrap("02501c7feba858c83e005acbf0505a892081288dcf7a8a37bd4fc47d7c24c799"));
+        checkpoints.put(    390000, Sha256Hash.wrap("8654451a7ed5286ba5c830cdf6e65cbbd7a77f650216541bfbe50af04933741b"));
+        checkpoints.put(    391285, Sha256Hash.wrap("76d13e8f08c2b7027251484078f734f91c485727031be6b4c21c42d5e103d0ad"));
 
         dnsSeeds = new String[] {
                 "seed1.syscoin.org",
